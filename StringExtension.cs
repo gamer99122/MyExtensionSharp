c3c2a4879f376580232eb07d8decbd679ad37ae2
gamer99122 <gamer99122@hotmail.com.tw>
2024-04-12 17:27:51 +0800

    Add StringExtension

---
+++ StringExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExtensionSharp
+{
+    public static class StringExtension
+    {
+
+		/// <summary>
+		/// Returns true if string is null or empty.
+		/// </summary>
+		public static bool pIsNullOrEmpty(this string s)
+		{
+			return string.IsNullOrEmpty(s);
+		}
+
+        public static string pSafeTrim(this string input)
+        {
+            // 檢查輸入是否為null或空字串
+            if (string.IsNullOrEmpty(input))
+            {
+                // 如果是，返回空字串
+                return string.Empty;
+            }
+            else
+            {
+                // 否則，執行trim操作
+                return input.Trim();
+            }
+        }
+
+
+    }
+}
